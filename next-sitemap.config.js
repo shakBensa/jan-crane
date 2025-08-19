8454b93e8c680a8b68e858f6abda15d49ece653c
Shaked Ben Samun <90576163+shakBensa@users.noreply.github.com>
2025-08-19 20:19:07 +0300

    Add files via upload

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://jan-manofim.co.il',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'weekly',
+  priority: 0.7,
+};
